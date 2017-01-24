3f0e70bfcedecb7d16b6b63c972a997fdcb7b893
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2017-01-24 13:56:56 +0200

    1.0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tariff_russianpost',
-    version='1.0.0',
+    name='tariff-russianpost',
+    version='1.0.1',
     packages=find_packages(),
     author='Dmitry Kalinin',
     author_email='vladimirovich@pbrf.ru',
